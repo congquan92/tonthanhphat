23d7b37ca3fbf864669744c6b79e7417dc9e8e26
congquan92 <145172432+congquan92@users.noreply.github.com>
2025-12-29 09:51:18 +0700

    fix: reposition welcome message in Home component

--- client/src/app/(client)/page.tsx
+++ client/src/app/(client)/page.tsx
@@ -3,8 +3,8 @@ import ProductCarousel from "@/components/product-carousel";
 export default function Home() {
     return (
         <div>
-            <h1>Welcome to Next.js!</h1>
             <ProductCarousel />
+            <h1>Welcome to Next.js!</h1>
         </div>
     );
 }
